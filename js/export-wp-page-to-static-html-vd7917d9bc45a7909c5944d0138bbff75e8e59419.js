f351d1bb71435ffa73fd18af2e8b535ad64d1f1e
gak1005 <gak1005@users.noreply.github.com>
2020-11-17 04:15:40 -0800

    delete

--- js/export-wp-page-to-static-html-vd7917d9bc45a7909c5944d0138bbff75e8e59419.js
+++
@@ -1,4 +0,0 @@
-/*!/wp-content/plugins/export-wp-page-to-static-html/public/js/export-wp-page-to-static-html-public.js*/
-(function($){'use strict'})(jQuery)
-;
-/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
