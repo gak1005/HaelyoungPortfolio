dffa86f0711c6f78926875867bb8953b58c43cf4
gak1005 <gak1005@users.noreply.github.com>
2020-11-17 03:59:03 -0800

    work upload

---
+++ js/export-wp-page-to-static-html-vd7917d9bc45a7909c5944d0138bbff75e8e59419.js
@@ -0,0 +1,4 @@
+/*!/wp-content/plugins/export-wp-page-to-static-html/public/js/export-wp-page-to-static-html-public.js*/
+(function($){'use strict'})(jQuery)
+;
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
